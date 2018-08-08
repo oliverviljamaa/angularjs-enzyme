f4dee4eb34b6e038be6682688f30c3d6d5a76056
Oliver Viljamaa <oliver.viljamaa@gmail.com>
2018-08-08 20:31:48 +0200

    Update test element wrapper spec

--- src/mount/TestElementWrapper/TestElementWrapper.spec.js
+++ src/mount/TestElementWrapper/TestElementWrapper.spec.js
@@ -1,51 +1,47 @@
 import 'angular';
 import 'angular-mocks';
+
 import TestElementWrapper from '.';
+import mount from '..';
 
 describe('Test element wrapper', () => {
-  const callback = jest.fn();
-  const TEMPLATE = `
-      <main>
-        <p class="first">Some text</p>
-        <div>
-          <button class="first" ng-click="$ctrl.callback()">Click me</button>
-          <button class="second">Or me</button>
-          <a href="https://transferwise.com" target="_blank">Or even me</a>
-        </div>
-      </main>
-    `;
-
-  let wrapper;
-  beforeEach(() => {
-    angular.module('some-module', []).component('someComponent', {
-      template: `
-        <h1>{{ $ctrl.title }}</h1>
-        <p>{{ $ctrl.text }}</p>
-      `,
-      bindings: { title: '<', text: '<' },
-    });
-    angular.mock.module('some-module');
-
-    wrapper = new TestElementWrapper(getAngularElement(TEMPLATE));
-  });
-
   describe('length', () => {
     it('is how many elements are in wrapper', () => {
-      const button = wrapper.find('button');
-      expect(button).toHaveLength(2);
+      const wrapper = mount(`
+        <main>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </main>
+      `);
+
+      const buttons = wrapper.find('li');
+
+      expect(buttons.length).toBe(3);
     });
   });
 
   describe('html', () => {
-    it('returns wrapper html without container element', () => {
+    it('returns html without container element', () => {
+      const wrapper = mount(`
+        <main>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </main>
+      `);
+
       const html = trimWhitespace(wrapper.html());
       const expectedHtml = trimWhitespace(`
-        <p class="first">Some text</p>
-        <div>
-          <button class="first" ng-click="$ctrl.callback()">Click me</button>
-          <button class="second">Or me</button>
-          <a href="https://transferwise.com" target="_blank">Or even me</a>
-        </div>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
       `);
 
       expect(html).toBe(expectedHtml);
@@ -53,104 +49,223 @@ describe('Test element wrapper', () => {
   });
 
   describe('text', () => {
-    it('returns wrapper text', () => {
+    it('returns text', () => {
+      const wrapper = mount(`
+        <main>
+          <h2>Some title</h2>
+          <p>Some text</p>
+        </main>
+      `);
       const text = trimWhitespace(wrapper.text());
-      expect(text).toBe('Some text Click me Or me Or even me');
+
+      expect(text).toBe('Some title Some text');
     });
   });
 
   describe('hasClass', () => {
+    let button;
+    beforeEach(() => {
+      const wrapper = mount(`
+        <main>
+          <button class="success">Pay</button>
+        </main>
+      `);
+      button = wrapper.find('button');
+    });
+
     it('returns true if wrapper has class', () => {
-      const p = wrapper.find('p');
-      expect(p.hasClass('first')).toBe(true);
+      expect(button.hasClass('success')).toBe(true);
     });
 
     it('returns false if wrapper does not have class', () => {
-      const p = wrapper.find('p');
-      expect(p.hasClass('second')).toBe(false);
+      expect(button.hasClass('error')).toBe(false);
     });
   });
 
   describe('exists', () => {
-    it('returns true when wrapper contains one or more elements', () => {
-      expect(wrapper.find('a').exists()).toBe(true);
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(`
+        <main>
+          <button class="success">Pay</button>
+        </main>
+      `);
     });
 
-    it('returns false when wrapper does not contain any elements', () => {
-      expect(wrapper.find('form').exists()).toBe(false);
+    it('returns true when one or more elements exist', () => {
+      expect(wrapper.find('button').exists()).toBe(true);
+    });
+
+    it('returns false when no elements exist', () => {
+      expect(wrapper.find('a').exists()).toBe(false);
     });
   });
 
   describe('find', () => {
-    it('allows finding by any query', () => {
-      const secondButton = wrapper.find('.first');
-      expect(secondButton.length).toBe(2);
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(`
+        <main>
+          <div class="wrong">
+            <a href="https://right.com">
+              Wrong
+            </a>
+          </div>
+          <div class="right">
+            <a href="https://wrong.com">
+              Wrong
+            </a>
+          </div>
+          <div class="right">
+            <a href="https://right.com">
+              Right
+            </a>
+          </div>
+        </main>
+      `);
     });
 
-    it('returns a test element wrapper for chainable operations', () => {
-      const link = wrapper.find('[href]');
-      expect(link).toBeInstanceOf(TestElementWrapper);
+    it('allows finding by any selector', () => {
+      const text = wrapper
+        .find('.right a[href="https://right.com"]')
+        .text()
+        .trim();
+
+      expect(text).toBe('Right');
+    });
+
+    it('returns test element wrapper for chaining', () => {
+      const links = wrapper.find('a');
+
+      expect(links).toBeInstanceOf(TestElementWrapper);
+    });
+  });
+
+  describe('map', () => {
+    it('maps over elements', () => {
+      const wrapper = mount(`
+        <main>
+          <ul>
+            <li>One</li>
+            <li>Two</li>
+            <li>Three</li>
+          </ul>
+        </main>
+      `);
+
+      const items = wrapper.find('li');
+
+      expect(items.map(item => item.text())).toEqual(['One', 'Two', 'Three']);
     });
   });
 
   describe('props', () => {
     it('returns props', () => {
+      const wrapper = mount(`
+        <main>
+          <a href="https://transferwise.com" target="_blank">Send money</a>
+        </main>
+      `);
+
       const link = wrapper.find('a');
+
       expect(link.props()).toEqual({ href: 'https://transferwise.com', target: '_blank' });
     });
   });
 
   describe('prop', () => {
     it('returns prop with key', () => {
+      const wrapper = mount(`
+        <main>
+          <a href="https://transferwise.com" target="_blank">Send money</a>
+        </main>
+      `);
+
       const link = wrapper.find('a');
+
       expect(link.prop('href')).toBe('https://transferwise.com');
     });
   });
 
   describe('simulate', () => {
-    it('simulates event', () => {
-      const buttonWithCallback = wrapper.find('button[ng-click]');
+    let wrapper;
+    let onClick;
+    beforeEach(() => {
+      onClick = jest.fn();
+      wrapper = mount(
+        `
+          <main>
+            <input ng-model="$ctrl.text" />
+            <p>{{ $ctrl.text }}</p>
+            <button ng-click="$ctrl.onClick($ctrl.text)">Click me</button>
+          </main>
+        `,
+        { text: 'Original text', onClick },
+      );
+    });
 
-      expect(callback).not.toBeCalled();
-      buttonWithCallback.simulate('click');
-      expect(callback).toBeCalled();
+    it('simulates event', () => {
+      const button = wrapper.find('button');
+
+      expect(onClick).not.toBeCalled();
+      button.simulate('click');
+      expect(onClick).toBeCalledWith('Original text');
+    });
+
+    it('returns wrapper itself for chaining', () => {
+      const button = wrapper.find('button');
+
+      expect(button.simulate('click')).toBe(button);
+    });
+
+    describe('for change', () => {
+      it('simulates change', () => {
+        const input = wrapper.find('input');
+
+        expect(wrapper.find('p').text()).toBe('Original text');
+        input.simulate('change', { target: { value: 'New text' } });
+        expect(wrapper.find('p').text()).toBe('New text');
+      });
+
+      it('throws error when data does not have proper event format', () => {
+        const input = wrapper.find('input');
+
+        expect(() => {
+          input.simulate('change', 'not an event format');
+        }).toThrowError();
+      });
     });
   });
 
   describe('setProps', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(
+        `
+          <main>
+            <h1>{{ $ctrl.title }}</h1>
+            <p>{{ $ctrl.text }}</p>
+          </main>
+        `,
+        { title: 'Original title', text: 'Original text' },
+      );
+    });
+
     it('sets props and updates view to reflect them', () => {
-      const template = `
-        <some-component
-          title="$ctrl.titleFromParent"
-          text="$ctrl.textFromParent"
-        ></some-component>
-      `;
-      wrapper = new TestElementWrapper(getAngularElement(template));
+      const title = () => wrapper.find('h1').text();
+      const text = () => wrapper.find('p').text();
 
-      const title = () => trimWhitespace(wrapper.find('h1').text());
-      const text = () => trimWhitespace(wrapper.find('p').text());
+      expect(title()).toBe('Original title');
+      expect(text()).toBe('Original text');
+      wrapper.setProps({ title: 'New title', text: 'New text' });
+      expect(title()).toBe('New title');
+      expect(text()).toBe('New text');
+    });
 
-      expect(title()).toBe('');
-      expect(text()).toBe('');
-      wrapper.setProps({ titleFromParent: 'A title', textFromParent: 'A text' });
-      expect(title()).toBe('A title');
-      expect(text()).toBe('A text');
+    it('returns wrapper itself for chaining', () => {
+      expect(wrapper.setProps({ title: 'New title' })).toBe(wrapper);
     });
   });
-
-  function getAngularElement(template) {
-    let $rootScope;
-    let angularElement;
-
-    angular.mock.inject(($compile, $injector) => {
-      $rootScope = $injector.get('$rootScope');
-      $rootScope.$ctrl = { callback };
-      angularElement = $compile(template)($rootScope);
-    });
-    $rootScope.$digest();
-
-    return angularElement;
-  }
 });
 
 function trimWhitespace(text) {
