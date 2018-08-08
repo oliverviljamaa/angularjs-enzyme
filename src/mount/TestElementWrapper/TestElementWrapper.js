c3aca56fb9df266438b312646b8252b51bf13f19
Oliver Viljamaa <oliver.viljamaa@gmail.com>
2018-08-08 20:30:36 +0200

    Use function to convert element to test element wrapper

--- src/mount/TestElementWrapper/TestElementWrapper.js
+++ src/mount/TestElementWrapper/TestElementWrapper.js
@@ -8,10 +8,12 @@ export default class TestElementWrapper {
 
     this.exists = () => angularElement && angularElement.length > 0;
 
-    this.find = query => {
-      const foundAngularElement = angular.element(angularElement[0].querySelectorAll(query));
-      return new TestElementWrapper(foundAngularElement);
+    this.find = selector => {
+      const foundElement = angularElement[0].querySelectorAll(selector);
+      return elementToTestElementWrapper(foundElement);
     };
+
+    this.map = fn => [...angularElement].map(elementToTestElementWrapper).map(fn);
 
     this.props = () =>
       [...angularElement[0].attributes].reduce(
@@ -21,8 +23,17 @@ export default class TestElementWrapper {
 
     this.prop = key => angularElement.attr(key);
 
-    this.simulate = event => {
+    this.simulate = (event, data) => {
+      if (event === 'change') {
+        if (!data || !data.target) {
+          throw new Error('Simulating change expects an event object ({ target: { value } })');
+        }
+        angularElement.val(data.target.value).triggerHandler('input');
+        return this;
+      }
       angularElement.triggerHandler(event);
+
+      return this;
     };
 
     this.setProps = props => {
@@ -30,10 +41,19 @@ export default class TestElementWrapper {
       angular.mock.inject($injector => {
         $rootScope = $injector.get('$rootScope');
       });
-      Object.entries(props).forEach(([key, value]) => {
-        $rootScope.$ctrl[key] = value;
-      });
+
+      $rootScope.$ctrl = {
+        ...$rootScope.$ctrl,
+        ...props,
+      };
+
       $rootScope.$digest();
+
+      return this;
     };
   }
+}
+
+function elementToTestElementWrapper(element) {
+  return new TestElementWrapper(angular.element(element));
 }
